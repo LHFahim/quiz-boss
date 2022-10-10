13649b2343f033ad07906e411a868bd31c83a115
Fahim-H <fahimlutful@gmail.com>
2022-10-10 23:58:11 +0600

    implemented toast

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,23 +1,51 @@
 import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Question = ({ question }) => {
-  // console.log(question);
+  console.log(question);
+  const notify = () => toast.success('You selected the correct answer!');
+
+  const checkAnswer = selectedAnswer => {
+    if (selectedAnswer === question.correctAnswer) {
+      notify();
+    }
+  };
+
   return (
     <div className="border p-5">
-      <h1 className="mb-5">{question.question}</h1>
+      <h1 className="mb-5 font-semibold text-indigo-600">
+        {question.question}
+      </h1>
       <div className="grid grid-cols-2 gap-y-5">
-        <h3 className="py-2 px-3 bg-indigo-300 rounded w-2/4 mx-auto">
+        <h3
+          onClick={() => checkAnswer(question.options[0])}
+          className="py-2 px-3 hover:bg-indigo-300 border rounded w-2/4 mx-auto"
+        >
           {question.options[0]}
         </h3>
-        <h3 className="py-2 px-3 bg-indigo-300 rounded w-2/4 mx-auto">
+
+        <h3
+          onClick={() => checkAnswer(question.options[1])}
+          className="py-2 px-3 hover:bg-indigo-300 border rounded w-2/4 mx-auto"
+        >
           {question.options[1]}
         </h3>
-        <h3 className="py-2 px-3 bg-indigo-300 rounded w-2/4 mx-auto">
+
+        <h3
+          onClick={() => checkAnswer(question.options[2])}
+          className="py-2 px-3 hover:bg-indigo-300 border rounded w-2/4 mx-auto"
+        >
           {question.options[2]}
         </h3>
-        <h3 className="py-2 px-3 bg-indigo-300 rounded w-2/4 mx-auto">
+
+        <h3
+          onClick={() => checkAnswer(question.options[3])}
+          className="py-2 px-3 hover:bg-indigo-300 border rounded w-2/4 mx-auto"
+        >
           {question.options[3]}
         </h3>
+        <ToastContainer />
       </div>
     </div>
   );
--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -3,13 +3,19 @@ import { useLoaderData } from 'react-router-dom';
 import Question from '../Question/Question';
 
 const Quiz = () => {
+  const quizName = useLoaderData().data.name;
   const quiz = useLoaderData().data.questions;
-  // console.log(quiz);
+
   return (
-    <div>
-      {quiz.map(question => (
-        <Question key={question.id} question={question} />
-      ))}
+    <div className="mt-10">
+      <h1 className="text-3xl font-bold mb-10 text-indigo-600">
+        {quizName} Quiz!
+      </h1>
+      <div className="space-y-5">
+        {quiz.map(question => (
+          <Question key={question.id} question={question} />
+        ))}
+      </div>
     </div>
   );
 };
