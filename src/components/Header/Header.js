b606dc27a9a918589190935401ebfc2996fd3dfc
Fahim-H <fahimlutful@gmail.com>
2022-10-11 01:47:24 +0600

    final push

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,17 +9,12 @@ const Header = () => {
       </div>
       <div>
         <p className="text-xl font-bold text-yellow-500">
-          Quizzes encourage pupils’ self awareness of progress and self
-          assessment By taking quizzes, pupils get instant feedback on their
+          Quizzes encourage students' self awareness of progress and self
+          assessment. By taking quizzes, students get instant feedback on their
           responses. And this can help them identify areas they need to develop
-          themselves and highlight progress for them to be proud of. In the
-          world of education there are many different ways to teach and to
-          consolidate what has been learned. In years gone by, children were
-          expected to memorise dates, formulae and figures by rote. But this
-          method of teaching can be quite dull, to say the least! One tool
-          becoming more common, especially in online education, is quizzes. In
-          this guide we’ll find out the many reasons why quizzes are valuable in
-          learning.
+          themselves and highlight progress for them to be proud of. Hence we
+          have designed our technological quizes in a way that will sharpen up
+          your programming skills.
         </p>
       </div>
     </div>
